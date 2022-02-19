a8d15697b32a8ac50144c52e3e4ed2c4e8c3ac15
Marcin Dłubis <marcindl@hotmail.com>
2022-02-19 11:58:38 +0100

    first

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const functions = require("./functions")
+const app = express();
+const port = 3000;
+const zz = 0;
+app.set("view engine","hbs");
+app.use("/assets", express.static(path.join(__dirname,"./assets")));
+app.use("/js", express.static(path.join(__dirname,"./js")));
+app.get("/", function (req,res){
+    res.render("index", {
+        pageTitle: "Lekcja ALK 19.02",
+        subTitle: "Podtytuł",
+        x: 4,
+        y: zz > 9 ? functions.add(1,2) : null
+    });
+});
+app.get("/about", function (req,res){
+    res.render("about");
+});
+
+app.listen(port, () => {
+    console.log(`Serwer działa na porcie ${port}`);
+});
